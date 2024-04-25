a26da9d5bae3361569c7c49efc0dcbf7ee2e2dc0
diegoandres11 <diegoa.sernac@uqvirtual.edu.co>
2024-04-25 17:29:40 -0500

    segunda pre-entrega mejoras

--- main.js
+++ main.js
@@ -9,29 +9,36 @@ let mascotas=[
     {id:7, especie:"pez", genero:"macho", edadAños:3, color:"griz"},
 ];
 
+
 //funcion que muetra en pantalla todas las opciones par que el usuario escoja
-const listar=(opciones, propiedad1, propiedad2, propiedad3, propiedad4) => "Ingrese el ID de la mascota que desea adoptar \n"+opciones.map( mascota =>"ID: "+mascota[propiedad1]+"  genero: "+mascota[propiedad2]+"  años: "+mascota[propiedad3]+"  color: "+mascota[propiedad4]).join("\n")
+const listar=(opciones, propiedad1, propiedad2, propiedad3, propiedad4) => "Ingrese el ID de la mascota que desea adoptar \n"+"Ingrese 0 para salir \n"+opciones.map( mascota =>"ID: "+mascota[propiedad1]+"  genero: "+mascota[propiedad2]+"  años: "+mascota[propiedad3]+"  color: "+mascota[propiedad4]).join("\n")
 
 
+let carrito=[]
+let idSeleccionado
+while(idSeleccionado!=0){
+    let animalBuscado = prompt("Ingrese la especie de animal que desea adoptar: ").toLowerCase()
+    let opciones = mascotas.filter(el => el.especie === animalBuscado)
+    idSeleccionado=parseInt(prompt(listar(opciones,"id", "genero", "edadAños", "color" )))
+    adoptar(mascotas,opciones,idSeleccionado)
+}
 
-let animalBuscado = prompt("Ingrese la especie de animal que desea adoptar: ").toLowerCase()
-let opciones = mascotas.filter(el => el.especie === animalBuscado)
-let idSeleccionado=parseInt(prompt(listar(opciones,"id", "genero", "edadAños", "color" )))
-adoptar(mascotas,opciones,idSeleccionado)
+
 
 
 //funcion que hace el proceso de la adopcion
-function adoptar(mascotas, opciones,id){
+function adoptar(mascotas, opciones, id) {
     if (opciones.length === 0) {
-        console.log("Lo siento, no hay animales de esa especie disponibles para adopción.")
+        alert("Lo siento, no hay animales de esa especie disponibles para adopción.");
     } else {
         let indice = mascotas.findIndex(el => el.id === id);
 
         if (indice === -1) {
-            alert("ID inválido. No se ha realizado la adopción.")
+            alert("ID inválido. No se ha realizado la adopción.");
         } else {
-            let animalAdoptado = mascotas.splice(indice, 1)[0]
-            alert(`¡Felicidades por la adopción! Ha adoptado un ${animalAdoptado.especie} ${animalAdoptado.genero}, de ${animalAdoptado.edadAños} años de edad, de color ${animalAdoptado.color}.`)
+            let animalAdoptado = mascotas.splice(indice, 1)[0];
+            carrito.push(animalAdoptado); // Agregar la mascota seleccionada al carrito
+            alert(`¡Felicidades por la adopción! Ha adoptado un ${animalAdoptado.especie} ${animalAdoptado.genero}, de ${animalAdoptado.edadAños} años de edad, de color ${animalAdoptado.color}.`);
         }
     }
 }
